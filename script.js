a969bf4ef0143124abbd6c42426c223faeb11c66
MSKstormzy <107139071+MSKstormzy@users.noreply.github.com>
2022-07-20 23:08:41 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var num = 7;
-
+var str = 'hello'
 
 document.getElementById('text').innerHTML = num;
+document.getElementById('text').addEventListener('click', function(){
+    document.getElementById('text').innerHTML = str;
+});
+
